3bb7a9353011e430ee1f8de99c36c35fbf14e458
prateekpranveer <prateekpranveer321@gmail.com>
2023-04-19 05:47:48 +0530

    replaced category desc with category

--- components/explore/Category.tsx
+++ components/explore/Category.tsx
@@ -8,6 +8,7 @@ interface PlaceProps {
     name: string,
     id: string,
     adress: string
+    slug:string
 }
 
 const Category = () => {
@@ -17,6 +18,7 @@ const Category = () => {
     const [places, setPlaces] = useState<Array<PlaceProps>>([])
     const [category, setCategory] = useState({
         title: '',
+        slug: '',
         desc: '',
         active: false,
     })
@@ -24,10 +26,11 @@ const Category = () => {
     const router = useRouter()
     const findAllPlaceByCategory = async () => {
         const query = router.query;
+        console.log(query)
         setisLoading(true)
         const queryData = await axios.get(`/category/${query.category}`)
         console.log(queryData.data);
-        setCategory({ ...category, title: queryData.data?.name, active: true })
+        setCategory({ ...category, title: queryData.data?.name, slug:queryData.data?.slug, active: true })
         const allPlaces = await axios.get(`/place/${queryData.data?._id}`)
         console.log(allPlaces.data)
         setPlaces(allPlaces.data)
@@ -39,22 +42,26 @@ const Category = () => {
     }, [router.query])
 
 
-    if (isLoading) {
+    if (isLoading ) {
         return (<div className='flex justify-center mt-20'><Spinner/></div>)
     }
-
         return (
             <div>
             <h1 className='mt-10 ml-2 font-rale-800 text-md'>{category.title}</h1>
-            <div className='mt-8 flex flex-wrap gap-8 cursor-pointer'>
+            <div className='mt-8 flex flex-wrap gap-8'>
                 {
                     places?.map((p, id) => (
                             <div key={id} className='max-w-fit relative bg-gray-200 rounded-full shadow-xl text-black'>
-                                <div className='absolute flex justify-center bg-black py-2 hover:bg-gray-700 rounded-full -top-2 shadow-lg cursor-pointer text-white font-jost-300 text-xs px-4'>
+                                <div onClick={() => {
+                                    router.push(`/${category.slug}/${p.slug}?activeTours`)
+                                }}
+                                 className='absolute flex justify-center bg-black py-2 hover:bg-gray-700 rounded-full -top-2 shadow-lg cursor-pointer text-white font-jost-300 text-xs px-4'>
                                     <span className='tracking-widest'>Explore Tours</span>
                                 </div>
-                                <div className='absolute bg-green-600 hover:bg-green-700 text-white p-3 rounded-full cursor-pointer -bottom-2 -right-2'><ChevronRight/></div>
-                                <div className='py-8 px-12 font-jost-300'>
+                                <div onClick={() => {
+                                    router.push(`/${category.slug}/${p.slug}`)
+                            }} className='absolute bg-green-600 hover:bg-green-700 text-white p-3 rounded-full cursor-pointer -bottom-2 -right-2'><ChevronRight/></div>
+                                <div className='py-8 cursor-pointer px-12 font-jost-300'>
                                     <h1 className='font-jost-300 text-3xl'>{p.name}</h1>
                                     <p>{p.adress}</p>
                                 </div>
--- components/search/SearchByCategory.tsx
+++ components/search/SearchByCategory.tsx
@@ -3,14 +3,21 @@ import { useRouter } from 'next/router';
 import axios from '../../axios';
 import Spinner from '../enhancement/Spinner';
 
+const skeleton = () => {
+    <div className='w-full h-4'></div>
+}
+
 
 
 const SearchByCategory = () => {
+    const [categoryIsLoading, setcategoryIsLoading] = React.useState(false)
     const [allCategory, setallCategory] = React.useState<any>([])
 
     const CategoryText = async () => {
+        setcategoryIsLoading(true)
         const allCategory = await axios.get('/category/all')
         setallCategory(allCategory.data)
+        setcategoryIsLoading(false)
     }
 
     useEffect(() => {
@@ -22,16 +29,16 @@ const SearchByCategory = () => {
         router.push(`/${p}`)
     }
 
-    if (!allCategory) return <Spinner/>
+    if (categoryIsLoading) return (<div className='flex justify-center mt-8'><Spinner/></div>)
     else 
     return (
         <div>
             <div className='flex flex-wrap gap-2'>
                 {
-                    allCategory?.map((p: { slug: string; desc: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }, id: React.Key | null | undefined) => (
+                    allCategory?.map((p: { slug: string; name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }, id: React.Key | null | undefined) => (
 
                         <div onClick={() => fetchPlaceByCategory(p.slug)} key={id} className='bg-gray-100 cursor-pointer hover:bg-gray-300 max-w-fit font-jost-300 py-1 px-4 rounded-full border border-gray-300'>
-                            <h1 className='text-sm'>{p.desc}</h1>
+                            <h1 className='text-sm'>{p.name}</h1>
                         </div>
 
                     ))
